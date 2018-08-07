af87bbf7259284ed845f7e4247d1f66a8be7ba0e
angus <lang@zjxu.org>
2018-08-07 21:15:44 +0800

    cors

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ const mongo = require('koa-mongo')
 const parser = require('koa-bodyparser')
 const app = new Koa()
 
+// --- [middlewares] --- 
 app.use(mongo(require('./conf').db))
-
 app.use(parser())
+app.use(async (ctx, next) => {
+    await next();
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+})
+// --- * ---
 
+// --- [routers] --- 
 const authRouter = require('./router/route-auth')
-
 app.use(authRouter.routes()).use(authRouter.allowedMethods())
+// --- * ---
 
 app.listen(3000)
